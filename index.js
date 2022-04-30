c0c0c0e581c5ff3e2f0dfeea0515616890584a8e
thavindu.ushan <thavinduushan@gmail.com>
2022-04-30 19:31:26 +0530

    create the request function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("req is commig");
+console.log("page is loading");
+
+function sendRequest() {
+  let city = document.getElementById("city").value;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "e04aba93d1mshc1808c971073845p15cefejsn186d3f4e99d8",
+      "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+    },
+  };
+
+  fetch("https://weatherapi-com.p.rapidapi.com/current.json?q=" + city, options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+}
