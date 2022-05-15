1dfb6fa0a506478a6ebcf95a4f8f275ecf219910
thavindu.ushan <thavinduushan@gmail.com>
2022-05-15 22:48:18 +0530

    created the http request

--- js/sports.js
+++ js/sports.js
@@ -1,3 +1,17 @@
 function sendRequest() {
-  console.log("test");
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "e04aba93d1mshc1808c971073845p15cefejsn186d3f4e99d8",
+      "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+    },
+  };
+
+  fetch(
+    "https://weatherapi-com.p.rapidapi.com/sports.json?q=Sri%20Lanka",
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
 }
